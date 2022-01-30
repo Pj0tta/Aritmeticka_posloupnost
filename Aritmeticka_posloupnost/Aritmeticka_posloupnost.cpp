81261d6c79863578de6a16d6389f935e96ecf56c
Petr Habel <petrhabel04@gmail.com>
2022-01-30 16:13:41 +0100

    Přidejte soubory projektu.

---
+++ Aritmeticka_posloupnost/Aritmeticka_posloupnost.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int clen = 0;
+    int diference = 0;
+    
+    printf("Zadej prvni clen posloupnosti ");
+    scanf_s("%d", &clen);
+
+    printf("Zadej diferenci mezi cleny ");
+    scanf_s("%d", &diference);
+
+    for (int i = 1; i <= 20; i++) {
+        printf("\nAritmeticka posloupnost je: %2d", clen);
+        clen += diference;
+    }
+    
+}
+
+
